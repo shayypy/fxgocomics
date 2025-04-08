a98d9ab211eba726f284b4ac005c27548a6bd649
shay <43248357+shayypy@users.noreply.github.com>
2025-04-07 20:35:10 -0500

    feat: api endpoint, real icon & followers

--- src/index.ts
+++ src/index.ts
@@ -25,61 +25,12 @@ const StripParams = z.object({
     .refine((n) => n >= 1 && n <= 31),
 });
 
-const ZodStrip = z.object({
-  title: z.string(),
-  canonicalUrl: z.string(),
-  imageUrl: z.string(),
-  published: z.string(),
-  series: z.object({
-    name: z.string(),
-    author: z.string(),
-    genre: z.ostring(),
-    language: z.ostring(),
-  }),
-}) satisfies z.ZodType<Strip>;
-
 const ZodSnowcodeStrip = z.object({
   c: z.string().min(1).max(100),
   d: z.string().regex(/\d{4}-\d{1,2}-\d{1,2}/),
 });
 
-app.get("/api/v1/:comic/:year/:month/:day", async (c) => {
-  const parsed = await StripParams.spa({
-    comic: c.req.param("comic"),
-    year: c.req.param("year"),
-    month: c.req.param("month"),
-    day: c.req.param("day"),
-  });
-  if (!parsed.success) {
-    return c.json(
-      { message: "Bad Request", errors: parsed.error.format() },
-      { status: 400 },
-    );
-  }
-
-  try {
-    const strip = await getStrip(
-      parsed.data.comic,
-      parsed.data.year,
-      parsed.data.month,
-      parsed.data.day,
-    );
-
-    return new Response(JSON.stringify(strip), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (e) {
-    console.error(e);
-    return c.json(
-      { message: "Internal Server Error", error: String(e) },
-      { status: 500 },
-    );
-  }
-});
-
+// Expected path syntax by Discord for Mastodon data
 app.get("/api/v1/statuses/:snowcode", async (c) => {
   const snowcode = c.req.param("snowcode");
   const decoded = await ZodSnowcodeStrip.parseAsync(decodeSnowcode(snowcode));
@@ -88,6 +39,7 @@ app.get("/api/v1/statuses/:snowcode", async (c) => {
     decoded.c,
     ...(decoded.d.split("-").map(Number) as [number, number, number]),
   );
+  const fallbackIconUrl = `${new URL(c.req.url).origin}/assets/fxgocomics-64w.png`;
 
   return c.json({
     id: snowcode,
@@ -128,13 +80,11 @@ app.get("/api/v1/statuses/:snowcode", async (c) => {
       discoverable: true,
       indexable: false,
       group: false,
-      avatar:
-        "https://gocomicscmsassets.gocomics.com/staging-assets/assets/Global_Feature_Badge_Peanuts_600_03e5d927b4.png?optimizer=image&width=1200&quality=75",
-      avatar_static:
-        "https://gocomicscmsassets.gocomics.com/staging-assets/assets/Global_Feature_Badge_Peanuts_600_03e5d927b4.png?optimizer=image&width=1200&quality=75",
+      avatar: strip.series.iconUrl ?? fallbackIconUrl,
+      avatar_static: strip.series.iconUrl ?? fallbackIconUrl,
       header: null,
       header_static: null,
-      followers_count: 0,
+      followers_count: strip.series.followers ?? 0,
       following_count: 0,
       statuses_count: 0,
       hide_collections: false,
@@ -174,8 +124,9 @@ app.get("/:comic/:year/:month/:day", async (c) => {
     );
   }
 
-  const { origin } = new URL(c.req.url);
-  if (!isPlatformRequest(c.req)) {
+  const { origin, host } = new URL(c.req.url);
+  const isDirectRequest = host.split(".")[0] === "d";
+  if (!isDirectRequest && !isPlatformRequest(c.req)) {
     return c.redirect(c.req.url.replace(origin, GOCOMICS_ORIGIN));
   }
 
@@ -194,17 +145,17 @@ app.get("/:comic/:year/:month/:day", async (c) => {
       { status: 500 },
     );
   }
+  if (isDirectRequest) {
+    return c.redirect(strip.imageUrl);
+  }
 
   const tags: MetaTag[] = [
     { name: "theme-color", value: "#2747B1" },
     { property: "og:image", content: strip.imageUrl },
-    // // { property: "og:description", content: `Published ${strip.published}, created by ${strip.series.author}` },
     { "http-equiv": "refresh", content: `0; url=${strip.canonicalUrl}` },
     { tagName: "link", rel: "canonical", href: strip.canonicalUrl },
     { property: "og:url", content: strip.canonicalUrl },
-    // { property: "twitter:site", content: strip.series.name },
-    // { property: "twitter:creator", content: strip.series.name },
-    // { property: "twitter:title", content: strip.series.name },
+    { property: "twitter:site", content: "gocomics" },
     { property: "twitter:card", content: "summary_large_image" },
     { property: "og:title", content: strip.series.name },
     { property: "og:site_name", content: "FxGoComics" },
@@ -256,6 +207,37 @@ app.get("/:comic/:year/:month/:day", async (c) => {
 			<p>Hello, you should be redirected shortly. If not, <a href="${strip.canonicalUrl}" rel="noreferrer">click here.</a></p>
 		</body></html>`,
   );
+});
+
+app.get("/api/v1/comics/:comic/strips/:date", async (c) => {
+  const parsed = await z
+    .object({
+      comic: z.string().min(1).max(100),
+      date: z.string().date(),
+    })
+    .spa({
+      comic: c.req.param("comic"),
+      date: c.req.param("date"),
+    });
+  if (!parsed.success) {
+    return c.json(
+      { message: "Bad Request", errors: parsed.error.format() },
+      { status: 400 },
+    );
+  }
+
+  const [year, month, day] = parsed.data.date.split("-").map(Number);
+  let strip: Strip;
+  try {
+    strip = await getStrip(parsed.data.comic, year, month, day);
+  } catch (e) {
+    console.error(e);
+    return c.json(
+      { message: "Internal Server Error", error: String(e) },
+      { status: 500 },
+    );
+  }
+  return c.json(strip);
 });
 
 export default app satisfies ExportedHandler<Env>;
